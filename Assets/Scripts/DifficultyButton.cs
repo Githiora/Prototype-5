8e808741677164cd7e9465b10b539dd09cbbc215
Githiora <gabriel.wamunyu@iconicmoments.co>
2024-11-01 15:04:01 +0300

    Use a parameter to change difficulty

--- Assets/Scripts/DifficultyButton.cs
+++ Assets/Scripts/DifficultyButton.cs
@@ -7,6 +7,8 @@ using UnityEngine.UI;
 
 public class DifficultyButton : MonoBehaviour
 {
+
+    public int difficulty;
     private Button button;
     private GameManager gameManager;
 
@@ -27,6 +29,6 @@ public class DifficultyButton : MonoBehaviour
     private void SetDifficulty()
     {
         Debug.Log(gameObject.name + " was clicked");
-        gameManager.StartGame();
+        gameManager.StartGame(difficulty);
     }
 }
