622411b8053f9a11786daac4acede58beaeeb136
Githiora <gabriel.wamunyu@iconicmoments.co>
2024-11-01 14:30:52 +0300

    Make your buttons start the game

--- Assets/Scripts/DifficultyButton.cs
+++ Assets/Scripts/DifficultyButton.cs
@@ -7,13 +7,14 @@ using UnityEngine.UI;
 
 public class DifficultyButton : MonoBehaviour
 {
-
     private Button button;
+    private GameManager gameManager;
 
     // Start is called before the first frame update
     void Start()
     {
         button = GetComponent<Button>();
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
         button.onClick.AddListener(SetDifficulty);
     }
 
@@ -26,5 +27,6 @@ public class DifficultyButton : MonoBehaviour
     private void SetDifficulty()
     {
         Debug.Log(gameObject.name + " was clicked");
+        gameManager.StartGame();
     }
 }
